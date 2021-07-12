5757c87dfafb968b205f6aeb3691635afff518e0
M Nurilman Baehaqi <mnurilmanbaehaqi@gmail.com>
2021-07-12 13:20:45 +0700

    fix error

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import axiosClient from "./src/network.js";
 
-let api = null;
-
-const ClickupSdk = {
-    api: null,
-    token: '',
-    setToken: (_token) => {
-        this.token = _token;
-        api = axiosClient(_token);
-    },
-    getUser: async (teamId, userId) => await api().get(`/team/${teamId}/user/${userId}`),
-    getTask: async (id) => await api().get(`/task/${id}/`),
+const ClickupSdk = (token) => {
+    const api = axiosClient(token);
+    return {
+        getUser: async (teamId, userId) => await api.get(`/team/${teamId}/user/${userId}`),
+        getTask: async (id) => await api.get(`/task/${id}/`),
+    }
 }
 
 export default ClickupSdk;
